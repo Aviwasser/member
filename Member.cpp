b93c4c04b82f0e5b53a0bca79169c19b4d263d44
Aviwasser <33319477+Aviwasser@users.noreply.github.com>
2018-04-12 01:21:26 +0300

    Create Member.cpp

---
+++ Member.cpp
@@ -0,0 +1,79 @@
+#include "Member.h"
+
+using namespace std;
+
+int Member::count = 0;	
+
+Member::~Member(){
+	
+	for(int i=0 ;i<following.size(); i++){
+		unfollow(*following[i]);
+	}
+	
+	for(int i=0 ;i<followers.size(); i++){
+		followers[i]->unfollow(*this);
+	}
+
+	count--;
+	following.clear();
+	followers.clear();
+	
+	
+}
+	
+Member::Member(){	
+	count++;
+}
+	
+ int Member::count(){
+	return count;
+}
+	
+	
+int Member::numOfFollowers(){
+	return this->followers.size();
+}
+int Member::numOfFollowing(){
+	return following.size();
+}
+	
+void Member::follow(Member& member){
+	Member *p = &member; 	
+	
+	if(p == this)
+		return;
+	
+	for(int i=0; i<following.size(); i++){
+		if(following[i] == p){
+			std::cout<<" here"<<endl;
+			return;
+		}
+	}	
+	following.push_back(p);
+	p->followers.push_back(this);	
+}
+	
+void Member::unfollow(Member& member){
+	
+	Member *p = &member; 
+	
+	if(p == this)
+		return;
+	
+	int flag = 0;
+	for(int i=0; i<following.size(); i++){
+		if(following[i] == p){
+			following.erase(following.begin() + i);
+			flag = 1;
+		}
+	}
+	if(flag == 0) return;
+	
+	for(int i=0; i< p->followers.size(); i++){
+		if(p->followers[i] == this){
+			p->followers.erase(p->followers.begin()+i);
+			return;
+		}
+	}
+	
+}
